a9fc3db47f2f8e1ae101d608dcfbe719b5e2642e
Thomas Vuille <thomas@newtomsoft.fr>
2020-12-31 10:20:32 +0100

    extraction GetConfiguration

--- Qwirkle.UI.Wpf/App.xaml.cs
+++ Qwirkle.UI.Wpf/App.xaml.cs
@@ -11,14 +11,8 @@ using Qwirkle.Infra.Persistence;
 using Qwirkle.Infra.Persistence.Adapters;
 using Qwirkle.UI.Wpf.Views;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Configuration;
-using Microsoft.EntityFrameworkCore;
 using System.IO;
 
 namespace Qwirkle.UI.Wpf
@@ -30,9 +24,7 @@ namespace Qwirkle.UI.Wpf
             base.OnStartup(startupEventArgs);
 
             ServiceCollection services = new ServiceCollection();
-
             services.AddScoped<MainWindow>();
-
             services.AddScoped<ICompliancePersistence, CompliancePersistenceAdapter>();
             services.AddScoped<IGamePersistence, GamePersistenceAdapter>();
             services.AddScoped<IPlayerPersistence, PlayerPersistenceAdapter>();
@@ -41,18 +33,22 @@ namespace Qwirkle.UI.Wpf
             services.AddScoped<IRequestGame, GameService>();
             services.AddScoped<IRequestPlayer, PlayerService>();
 
-            var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName)
-            .AddJsonFile(@"..\sharesettings.Development.json", optional: true) //When dev
-            .AddJsonFile("sharesettings.Development.json", optional: true) //When published
-            .AddJsonFile(@"D:\Boulot\projets info\Qwirkle\sharesettings.Development.json", optional: true); //TODO!!!
-            IConfigurationRoot configuration = builder.Build();
+            EntityFrameworkTools<DefaultDbContext>.AddDbContext(services, GetConfiguration());
 
-            EntityFrameworkTools<DefaultDbContext>.AddDbContext(services, configuration);
-
-            ServiceProvider serviceProvider = services.BuildServiceProvider();
-            MainWindow mainWindow = serviceProvider.GetService<MainWindow>();
+            var serviceProvider = services.BuildServiceProvider();
+            var mainWindow = serviceProvider.GetService<MainWindow>();
             mainWindow.Show();
+        }
+
+        private static IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName) //todo not work!!!
+                        .AddJsonFile(@"..\sharesettings.Development.json", optional: true) //When dev //TODO not work!!!
+                        .AddJsonFile("sharesettings.Development.json", optional: true) //When published //TODO not work!!!
+                        .AddJsonFile(@"D:\Boulot\projets info\Qwirkle\sharesettings.Development.json", optional: true); //TODO!!!
+            var configuration = builder.Build();
+            return configuration;
         }
     }
 }
