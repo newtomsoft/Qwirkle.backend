f2523032d5c9e550714fac09244d12ca88b059a1
Thomas Vuille <thomas@newtomsoft.fr>
2021-11-11 17:56:42 +0100

    add domain User ; using UserId

--- Qwirkle.Core/Entities/Player.cs
+++ Qwirkle.Core/Entities/Player.cs
@@ -1,55 +1,47 @@
 ﻿namespace Qwirkle.Core.Entities;
 public class Player
 {
-    private bool _gameTurn;
-
-    public int Id { get; set; }
-    public string Pseudo { get; set; }
-    public int GameId { get; set; }
+    public int Id { get; }
+    public int UserId { get; set; }
+    public string Pseudo { get; }
+    public int GameId { get; }
     public int GamePosition { get; set; }
     public int Points { get; set; }
     public int LastTurnPoints { get; set; }
-    public Rack Rack { get; set; }
+    public Rack Rack { get; }
     public bool LastTurnSkipped { get; set; }
+    public bool IsTurn { get; private set; }
 
-    public Player(int id, int gameId, string pseudo, int gamePosition, int points, int lastTurnPoints, List<TileOnPlayer> tiles, bool turn, bool lastTurnSkipped) // todo remplacer tiles par rack
+    public Player(int id, int userId, int gameId, string pseudo, int gamePosition, int points, int lastTurnPoints, Rack rack, bool isTurn, bool lastTurnSkipped)
     {
         Id = id;
+        UserId = userId;
         GameId = gameId;
         Pseudo = pseudo;
         GamePosition = gamePosition;
         Points = points;
         LastTurnPoints = lastTurnPoints;
-        Rack = new Rack(tiles);
-        _gameTurn = turn;
+        Rack = rack;
+        IsTurn = isTurn;
         LastTurnSkipped = lastTurnSkipped;
     }
 
-    public bool IsTurn => _gameTurn;
-
-
-    public void SetTurn(bool turn) => _gameTurn = turn;
-    public bool HasTiles(IEnumerable<int> tilesIds)
-    {
-        var playerTilesId = Rack.Tiles.Select(t => t.Id);
-        return tilesIds.All(id => playerTilesId.Contains(id));
-    }
+    public void SetTurn(bool turn) => IsTurn = turn;
+    public bool HasTiles(IEnumerable<int> tilesIds) => tilesIds.All(id => Rack.Tiles.Select(t => t.Id).Contains(id));
 
     public int TilesNumberCanBePlayedAtGameBeginning()
     {
         var tiles = Rack.Tiles;
-        int maxSameColor = 0;
-        int maxSameShape = 0;
-        for (int i = 0; i < tiles.Count; i++)
+        var maxSameColor = 0;
+        var maxSameShape = 0;
+        for (var i = 0; i < tiles.Count; i++)
         {
-            int sameColor = 0;
-            int sameShape = 0;
-            for (int j = i + 1; j < tiles.Count; j++)
+            var sameColor = 0;
+            var sameShape = 0;
+            for (var j = i + 1; j < tiles.Count; j++)
             {
-                if (tiles[i].Color == tiles[j].Color && tiles[i].Shape != tiles[j].Shape)
-                    sameColor++;
-                if (tiles[i].Color != tiles[j].Color && tiles[i].Shape == tiles[j].Shape)
-                    sameShape++;
+                if (tiles[i].Color == tiles[j].Color && tiles[i].Shape != tiles[j].Shape) sameColor++;
+                if (tiles[i].Color != tiles[j].Color && tiles[i].Shape == tiles[j].Shape) sameShape++;
             }
             maxSameColor = Math.Max(maxSameColor, sameColor);
             maxSameShape = Math.Max(maxSameShape, sameShape);
