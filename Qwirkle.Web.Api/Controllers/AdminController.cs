fc92c14a67e858a4582d932204ce5312e202e2d2
Thomas Vuille <thomas@newtomsoft.fr>
2021-11-12 12:27:34 +0100

    CreateGame, GetUserGames

---
+++ Qwirkle.Web.Api/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿namespace Qwirkle.Web.Api.Controllers;
+
+[ApiController]
+[Route("Admin")]
+public class AdminController : ControllerBase
+{
+    private readonly ILogger<GameController> _logger;
+    private readonly CoreUseCase _coreUseCase;
+    private readonly UserManager<UserDao> _userManager;
+    private int UserId => int.Parse(_userManager.GetUserId(User) ?? "0");
+
+    public AdminController(ILogger<GameController> logger, CoreUseCase coreUseCase, UserManager<UserDao> userManager)
+    {
+        _logger = logger;
+        _coreUseCase = coreUseCase;
+        _userManager = userManager;
+    }
+
+    [HttpPost("GamesByUserId/{userId:int}")]
+    public ActionResult<int> GetGamesByUserId(int userId) => new ObjectResult(_coreUseCase.GetUserGames(userId));
+
+
+    [HttpGet("GamesIds")]
+    public ActionResult<int> GetGamesIdsContainingPlayers() => new ObjectResult(_coreUseCase.GetGamesIdsContainingPlayers());
+}
--- Qwirkle.Web.Api/Controllers/GameController.cs
+++ Qwirkle.Web.Api/Controllers/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : ControllerBase
     private readonly ILogger<GameController> _logger;
     private readonly CoreUseCase _coreUseCase;
     private readonly UserManager<UserDao> _userManager;
+    private int UserId => int.Parse(_userManager.GetUserId(User) ?? "0");
 
     public GameController(ILogger<GameController> logger, CoreUseCase coreUseCase, UserManager<UserDao> userManager)
     {
@@ -19,6 +20,8 @@ public class GameController : ControllerBase
     [HttpPost("New")]
     public ActionResult<int> CreateGame(List<int> usersIds)
     {
+        if (UserId == 0) return NotFound();
+        usersIds.Add(UserId);
         _logger.LogInformation($"CreateGame with {usersIds}");
         return new ObjectResult(_coreUseCase.CreateGame(usersIds));
     }
@@ -28,14 +31,6 @@ public class GameController : ControllerBase
     public ActionResult<int> GetGame(int gameId) => new ObjectResult(_coreUseCase.GetGame(gameId));
 
 
-    [HttpPost("GamesByUserId/{userId:int}")]
-    public ActionResult<int> GetGamesByUserId(int userId) => new ObjectResult(_coreUseCase.GetUserGames(userId));
-
-
     [HttpGet("UserGames")]
-    public ActionResult<int> GetUserGames() => new ObjectResult(_coreUseCase.GetUserGames(int.Parse(_userManager.GetUserId(User) ?? "0")));
-
-
-    [HttpGet("GamesIds")]
-    public ActionResult<int> GetGamesIdsContainingPlayers() => new ObjectResult(_coreUseCase.GetGamesIdsContainingPlayers());
+    public ActionResult<int> GetUserGames() => new ObjectResult(_coreUseCase.GetUserGames(UserId));
 }
