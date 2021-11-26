03434edd35e98cbcc0a563df0cad1f1eebec1255
Thomas Vuille <thomas@newtomsoft.fr>
2021-11-26 21:00:51 +0100

    using static constructor

--- Qwirkle.Core/Entities/TileOnPlayer.cs
+++ Qwirkle.Core/Entities/TileOnPlayer.cs
@@ -4,10 +4,7 @@ public class TileOnPlayer : Tile
 {
     public RackPosition RackPosition { get; }
 
-    public TileOnPlayer(RackPosition rackPosition, int id, TileColor color, TileShape shape) : base(id, color, shape)
-    {
-        RackPosition = rackPosition;
-    }
+    public TileOnPlayer(RackPosition rackPosition, int id, TileColor color, TileShape shape) : base(id, color, shape) => RackPosition = rackPosition;
 
-    public TileOnBoard ToTileOnBoard(Coordinates coordinates) => new TileOnBoard(this, coordinates);
+    public TileOnBoard ToTileOnBoard(Coordinates coordinates) => TileOnBoard.From(this, coordinates);
 }
