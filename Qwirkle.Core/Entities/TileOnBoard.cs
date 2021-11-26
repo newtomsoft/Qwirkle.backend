6c160e82d271a140d8c7fab481913b3a2ddb2e32
Thomas Vuille <thomas@newtomsoft.fr>
2021-11-26 21:00:26 +0100

    add private static contructor and this From

--- Qwirkle.Core/Entities/TileOnBoard.cs
+++ Qwirkle.Core/Entities/TileOnBoard.cs
@@ -5,9 +5,9 @@ public class TileOnBoard : Tile
     public Coordinates Coordinates { get; }
 
     public TileOnBoard(Tile tile, Coordinates coordinates) : base(tile) => Coordinates = coordinates;
-
     public TileOnBoard(int id, TileColor color, TileShape shape, Coordinates coordinates) : base(id, color, shape) => Coordinates = coordinates;
-
-    public TileOnBoard(TileColor color, TileShape shape, Coordinates coordinates) : this(0, color, shape, coordinates)
-    { }
+    public TileOnBoard(TileColor color, TileShape shape, Coordinates coordinates) : this(0, color, shape, coordinates) { }
+    public static TileOnBoard From(TileOnPlayer tile, Coordinates coordinates) => new (tile, coordinates);
+    
+    private TileOnBoard(TileOnPlayer tile, Coordinates coordinates) : base(tile) => Coordinates = coordinates;
 }
