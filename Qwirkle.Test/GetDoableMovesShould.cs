54b8620a1f65a0c2a4971740c4429099d12a4b09
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-04 17:49:24 +0100

    refactoring test

--- Qwirkle.Test/GetDoableMovesShould.cs
+++ Qwirkle.Test/GetDoableMovesShould.cs
@@ -2,38 +2,40 @@
 
 public class GetDoableMovesShould
 {
-    private readonly DefaultDbContext _dbContext;
-    private readonly InfoUseCase _infoUseCase;
-    private readonly CoreUseCase _useCase;
-    private readonly BotUseCase _botUseCase;
-
+    #region constructor and privates
     public GetDoableMovesShould()
     {
         var connectionFactory = new ConnectionFactory();
         _dbContext = connectionFactory.CreateContextForInMemory();
         connectionFactory.Add4DefaultTestUsers();
-
         var repository = new Repository(_dbContext);
-        _infoUseCase = new InfoUseCase(repository, null);
-        _useCase = new CoreUseCase(repository, null, _infoUseCase);
-        _botUseCase = new BotUseCase(_infoUseCase, _useCase);
+        var infoUseCase = new InfoUseCase(repository, null);
+        var useCase = new CoreUseCase(repository, null, infoUseCase);
+        var usersIds = infoUseCase.GetAllUsersId();
+        var players = useCase.CreateGame(usersIds).OrderBy(p => p.Id).ToList();
+        _botUseCase = new BotUseCase(infoUseCase, useCase, null);
+        _player = players[0];
+        _userId = usersIds[0];
     }
 
-    #region private methods
+    private readonly DefaultDbContext _dbContext;
+    private readonly BotUseCase _botUseCase;
+    private readonly Player _player;
+    private readonly int _userId;
+
     private void ChangePlayerTilesBy(int playerId, IReadOnlyList<TileDao> newTiles)
     {
         var tilesOnPlayer = _dbContext.TilesOnPlayer.Where(t => t.PlayerId == playerId).ToList();
         for (var i = 0; i < 6; i++) tilesOnPlayer[i].TileId = newTiles[i].Id;
         _dbContext.SaveChanges();
     }
+
+    private static List<List<TileOnBoard>> TilesCombination(int tilesNumberInCombo, IEnumerable<PlayReturn> playReturns) => playReturns.Where(p => p.TilesPlayed.Count == tilesNumberInCombo).Select(p => p.TilesPlayed).ToList();
     #endregion
 
     [Fact]
-    public void Return6ItemsWhenBoardIsEmptyAndNoCombinationPossibleInRack()
+    public void Return6NoComboTilesAnd0Combo2TilesWhenBoardIsEmptyAndNoCombinationPossibleInRack()
     {
-        var usersIds = _infoUseCase.GetAllUsersId();
-        var players = _useCase.CreateGame(usersIds);
-        players = players.OrderBy(p => p.Id).ToList();
         var constTile0 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Circle);
         var constTile1 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Blue && t.Shape == TileShape.Clover);
         var constTile2 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Orange && t.Shape == TileShape.Diamond);
@@ -41,28 +43,25 @@ public class GetDoableMovesShould
         var constTile4 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Red && t.Shape == TileShape.FourPointStar);
         var constTile5 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Yellow && t.Shape == TileShape.Square);
         var constTiles = new List<TileDao> { constTile0!, constTile1!, constTile2!, constTile3!, constTile4!, constTile5! }.OrderBy(t => t.Id).ToList();
-        ChangePlayerTilesBy(players[0].Id, constTiles);
+        ChangePlayerTilesBy(_player.Id, constTiles);
 
-        var gameId = players[0].GameId;
-        var playReturns = _botUseCase.ComputeDoableMoves(gameId, usersIds[0]);
-        playReturns.Count.ShouldBe(6); // 6 tiles from the rack are all doable
+        var playReturns = _botUseCase.ComputeDoableMoves(_player.GameId, _userId);
 
-        var playReturnsWith1Tile = playReturns.Where(p => p.TilesPlayed.Count == 1).ToList();
-        var tilesPlayedSingle = playReturnsWith1Tile.Select(p => p.TilesPlayed[0]).OrderBy(t => t.Color).ThenBy(t => t.Shape).ToList();
-        playReturnsWith1Tile.Count.ShouldBe(6); // 6 tiles from the rack are all doable
-        tilesPlayedSingle.Select(t => t.Coordinates).ShouldAllBe(c => c == Coordinates.From(0, 0)); // all in coordinates (0,0)
-        tilesPlayedSingle.Select(t => t.ToTile()).SequenceEqual(constTiles.Select(t => t.ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape)).ShouldBeTrue();// each of tile from rack
+        var noComboTile = TilesCombination(1, playReturns);
+        noComboTile.Count.ShouldBe(6); // 6 tiles from the rack are all doable
+        noComboTile.Select(t => t[0].Coordinates).ShouldAllBe(c => c == Coordinates.From(0, 0)); // all in coordinates (0,0)
+        noComboTile.Select(t => t[0].ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape).SequenceEqual(constTiles.Select(t => t.ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape)).ShouldBeTrue();// each of tile from rack
 
-        var playReturnsWith2Tiles = playReturns.Where(p => p.TilesPlayed.Count == 2).ToList();
-        playReturnsWith2Tiles.Count.ShouldBe(0); // no combination possible with these 6 tiles
+        var combo2Tiles = TilesCombination(2, playReturns);
+        combo2Tiles.Count.ShouldBe(0); // no combination possible
+
+        var combo3Tiles = TilesCombination(3, playReturns);
+        combo3Tiles.Count.ShouldBe(0); // no combination possible
     }
 
     [Fact]
     public void ReturnMaxItemsWhenBoardIsEmptyAndMaxCombinationInRackIsPossible()
     {
-        var usersIds = _infoUseCase.GetAllUsersId();
-        var players = _useCase.CreateGame(usersIds);
-        players = players.OrderBy(p => p.Id).ToList();
         var constTile0 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Circle);
         var constTile1 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Clover);
         var constTile2 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Diamond);
@@ -70,33 +69,25 @@ public class GetDoableMovesShould
         var constTile4 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.FourPointStar);
         var constTile5 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Square);
         var constTiles = new List<TileDao> { constTile0!, constTile1!, constTile2!, constTile3!, constTile4!, constTile5! }.OrderBy(t => t.Id).ToList();
-        ChangePlayerTilesBy(players[0].Id, constTiles);
+        ChangePlayerTilesBy(_player.Id, constTiles);
 
-        var gameId = players[0].GameId;
-        var playReturns = _botUseCase.ComputeDoableMoves(gameId, usersIds[0]);
+        var playReturns = _botUseCase.ComputeDoableMoves(_player.GameId, _userId);
 
-        var playReturnsWith1Tile = playReturns.Where(p => p.TilesPlayed.Count == 1).ToList();
-        var tilesPlayedSingle = playReturnsWith1Tile.Select(p => p.TilesPlayed[0]).OrderBy(t => t.Color).ThenBy(t => t.Shape).ToList();
+        var noComboTile = TilesCombination(1, playReturns);
+        noComboTile.Count.ShouldBe(6); // 6 tiles from the rack are all doable
+        noComboTile.Select(t => t[0].Coordinates).ShouldAllBe(c => c == Coordinates.From(0, 0)); // all in coordinates (0,0)
+        noComboTile.Select(t => t[0].ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape).SequenceEqual(constTiles.Select(t => t.ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape)).ShouldBeTrue();// each of tile from rack
 
-        playReturnsWith1Tile.Count.ShouldBe(6); // 6 tiles from the rack are all doable
-        tilesPlayedSingle.Select(t => t.Coordinates).ShouldAllBe(c => c == Coordinates.From(0, 0)); // all in coordinates (0,0)
-        tilesPlayedSingle.Select(t => t.ToTile()).SequenceEqual(constTiles.Select(t => t.ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape)).ShouldBeTrue();// each of tile from rack
+        var combo2Tiles = TilesCombination(2, playReturns);
+        combo2Tiles.Count.ShouldBe(6 * 5); // 6 first tile x 5 second tile
 
-        var playReturnsWith2Tiles = playReturns.Where(p => p.TilesPlayed.Count == 2).ToList();
-        var tilesPlayedWith2Tiles = playReturnsWith2Tiles.Select(p => p.TilesPlayed).ToList();
-        tilesPlayedWith2Tiles.Count.ShouldBe(6 * 5); // 6 first tile x 5 second tile
-
-        var playReturnsWith3Tiles = playReturns.Where(p => p.TilesPlayed.Count == 3).ToList();
-        var tilesPlayedWith3Tiles = playReturnsWith3Tiles.Select(p => p.TilesPlayed).ToList();
-        tilesPlayedWith3Tiles.Count.ShouldBe(6 * 5 * 4); // 6 first tile x 5 second tile x 4 third tile
+        var combo3Tiles = TilesCombination(3, playReturns);
+        combo3Tiles.Count.ShouldBe(6 * 5 * 4); // 6 first tile x 5 second tile x 4 third tile
     }
 
     [Fact]
-    public void TORENAME()
+    public void ReturnOtherItemsWhenBoardIsEmptyAndOtherRack()
     {
-        var usersIds = _infoUseCase.GetAllUsersId();
-        var players = _useCase.CreateGame(usersIds);
-        players = players.OrderBy(p => p.Id).ToList();
         var constTile0 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Circle);
         var constTile1 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Yellow && t.Shape == TileShape.Clover);
         var constTile2 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Diamond);
@@ -104,33 +95,25 @@ public class GetDoableMovesShould
         var constTile4 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Red && t.Shape == TileShape.FourPointStar);
         var constTile5 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Square);
         var constTiles = new List<TileDao> { constTile0!, constTile1!, constTile2!, constTile3!, constTile4!, constTile5! }.OrderBy(t => t.Id).ToList();
-        ChangePlayerTilesBy(players[0].Id, constTiles);
+        ChangePlayerTilesBy(_player.Id, constTiles);
 
-        var gameId = players[0].GameId;
-        var playReturns = _botUseCase.ComputeDoableMoves(gameId, usersIds[0]);
+        var playReturns = _botUseCase.ComputeDoableMoves(_player.GameId, _userId);
 
-        var playReturnsWith1Tile = playReturns.Where(p => p.TilesPlayed.Count == 1).ToList();
-        var tilesPlayedSingle = playReturnsWith1Tile.Select(p => p.TilesPlayed[0]).OrderBy(t => t.Color).ThenBy(t => t.Shape).ToList();
+        var noComboTile = TilesCombination(1, playReturns);
+        noComboTile.Count.ShouldBe(6); // 6 tiles from the rack are all doable
+        noComboTile.Select(t => t[0].Coordinates).ShouldAllBe(c => c == Coordinates.From(0, 0)); // all in coordinates (0,0)
+        noComboTile.Select(t => t[0].ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape).SequenceEqual(constTiles.Select(t => t.ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape)).ShouldBeTrue();// each of tile from rack
+                                                                                                                                                                                                      // 
+        var combo2Tiles = TilesCombination(2, playReturns);
+        combo2Tiles.Count.ShouldBe(3 * 2); // 3 first tile x 2 second tile
 
-        playReturnsWith1Tile.Count.ShouldBe(6); // 6 tiles from the rack are all doable
-        tilesPlayedSingle.Select(t => t.Coordinates).ShouldAllBe(c => c == Coordinates.From(0, 0)); // all in coordinates (0,0)
-        tilesPlayedSingle.Select(t => t.ToTile()).SequenceEqual(constTiles.Select(t => t.ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape)).ShouldBeTrue();// each of tile from rack
-
-        var playReturnsWith2Tiles = playReturns.Where(p => p.TilesPlayed.Count == 2).ToList();
-        var tilesPlayedWith2Tiles = playReturnsWith2Tiles.Select(p => p.TilesPlayed).ToList();
-        tilesPlayedWith2Tiles.Count.ShouldBe(3 * 2); // 3 first tile x 2 second tile
-
-        var playReturnsWith3Tiles = playReturns.Where(p => p.TilesPlayed.Count == 3).ToList();
-        var tilesPlayedWith3Tiles = playReturnsWith3Tiles.Select(p => p.TilesPlayed).ToList();
-        tilesPlayedWith3Tiles.Count.ShouldBe(3 * 2 * 1); // 3 first tile x 2 second tile x 1 third tile
+        var combo3Tiles = TilesCombination(3, playReturns);
+        combo3Tiles.Count.ShouldBe(3 * 2 * 1); // 3 first tile x 2 second tile x 1 third tile
     }
 
     [Fact]
-    public void TORENAME2()
+    public void Return3Combos2GreenTiles()
     {
-        var usersIds = _infoUseCase.GetAllUsersId();
-        var players = _useCase.CreateGame(usersIds);
-        players = players.OrderBy(p => p.Id).ToList();
         var constTile0 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Circle);
         var constTile1 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Yellow && t.Shape == TileShape.Clover);
         var constTile2 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Diamond);
@@ -138,24 +121,19 @@ public class GetDoableMovesShould
         var constTile4 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Red && t.Shape == TileShape.Diamond);
         var constTile5 = _dbContext.Tiles.FirstOrDefault(t => t.Color == TileColor.Green && t.Shape == TileShape.Square);
         var constTiles = new List<TileDao> { constTile0!, constTile1!, constTile2!, constTile3!, constTile4!, constTile5! }.OrderBy(t => t.Id).ToList();
-        ChangePlayerTilesBy(players[0].Id, constTiles);
+        ChangePlayerTilesBy(_player.Id, constTiles);
 
-        var gameId = players[0].GameId;
-        var playReturns = _botUseCase.ComputeDoableMoves(gameId, usersIds[0]);
+        var playReturns = _botUseCase.ComputeDoableMoves(_player.GameId, _userId);
 
-        var playReturnsWith1Tile = playReturns.Where(p => p.TilesPlayed.Count == 1).ToList();
-        var tilesPlayedSingle = playReturnsWith1Tile.Select(p => p.TilesPlayed[0]).OrderBy(t => t.Color).ThenBy(t => t.Shape).ToList();
+        var noComboTile = TilesCombination(1, playReturns);
+        noComboTile.Count.ShouldBe(6); // 6 tiles from the rack are all doable
+        noComboTile.Select(t => t[0].Coordinates).ShouldAllBe(c => c == Coordinates.From(0, 0)); // all in coordinates (0,0)
+        noComboTile.Select(t => t[0].ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape).SequenceEqual(constTiles.Select(t => t.ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape)).ShouldBeTrue();// each of tile from rack
 
-        playReturnsWith1Tile.Count.ShouldBe(6); // 6 tiles from the rack are all doable
-        tilesPlayedSingle.Select(t => t.Coordinates).ShouldAllBe(c => c == Coordinates.From(0, 0)); // all in coordinates (0,0)
-        tilesPlayedSingle.Select(t => t.ToTile()).SequenceEqual(constTiles.Select(t => t.ToTile()).OrderBy(t => t.Color).ThenBy(t => t.Shape)).ShouldBeTrue();// each of tile from rack
+        var combo2Tiles = TilesCombination(2, playReturns);
+        combo2Tiles.Count.ShouldBe((3 + 1 + 1) * 2); // (3 combo greens + 1 combo circle + 1 combo diamond) *2
 
-        var playReturnsWith2Tiles = playReturns.Where(p => p.TilesPlayed.Count == 2).ToList();
-        var tilesPlayedWith2Tiles = playReturnsWith2Tiles.Select(p => p.TilesPlayed).ToList();
-        tilesPlayedWith2Tiles.Count.ShouldBe(10);
-
-        var playReturnsWith3Tiles = playReturns.Where(p => p.TilesPlayed.Count == 3).ToList();
-        var tilesPlayedWith3Tiles = playReturnsWith3Tiles.Select(p => p.TilesPlayed).ToList();
-        tilesPlayedWith3Tiles.Count.ShouldBe(3 * 2 * 1); // 3 first tile x 2 second tile x 1 third tile
+        var combo3Tiles = TilesCombination(3, playReturns);
+        combo3Tiles.Count.ShouldBe(3 * 2 * 1); // 3 first tile x 2 second tile x 1 third tile
     }
 }
