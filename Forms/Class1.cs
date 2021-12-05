20457caae55812b385f0df89224b35b5c585e4f4
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-05 10:42:26 +0100

    try BrowserFetcher wip

---
+++ Forms/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms
+{
+    internal class Class1
+    {
+        public void Whatever()
+        {
+            WebBrowser wb = new WebBrowser();
+            wb.DocumentCompleted += Wb_DocumentCompleted;
+
+            wb.ScriptErrorsSuppressed = true;
+            wb.Navigate("http://stackoverflow.com");
+        }
+
+        private void Wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var wb = (WebBrowser)sender;
+
+            var html = wb.Document.GetElementsByTagName("HTML")[0].OuterHtml;
+            var domd = wb.Document.GetElementById("copyright").InnerText;
+            /* ... */
+        }
+    }
+}
--- Qwirkle.UltraBoardGames.Player/Program.cs
+++ Qwirkle.UltraBoardGames.Player/Program.cs
@@ -1,24 +1,52 @@
-﻿using System.Diagnostics;
-using HtmlAgilityPack;
+﻿using HtmlAgilityPack;
+using PuppeteerSharp;
 
 Console.WriteLine("scraping program");
 
-var url = "https://www.ultraboardgames.com/qwirkle/game.php?startcomputer";
-var web = new HtmlWeb();
-var doc = web.Load(url);
+const string gamePageUrl = "https://www.ultraboardgames.com/qwirkle/game.php?startcomputer";
+//var web = new HtmlWeb();
+//var doc = web.Load(gamePageUrl);
 
 //< img src = "gamegfx/p4.png" class= "ui-draggable ui-draggable-handle" style = "position: relative;" >
 //class="ui-droppable"
-var scorePlayer = int.Parse(doc.GetElementbyId("score_player").InnerText);
-var scoreComputer = int.Parse(doc.GetElementbyId("score_computer").InnerText);
+//var scorePlayer = int.Parse(doc.GetElementbyId("score_player").InnerText);
+//var scoreComputer = int.Parse(doc.GetElementbyId("score_computer").InnerText);
 
+//var process = new Process();
+//process.StartInfo.UseShellExecute = true;
+//process.StartInfo.FileName = url;
+//process.Start();
 
-Process.Start("nppp\\notepad++.exe", "args");
+//Process.Start(url);
+//Process.Start("nppp\\notepad++.exe", "args");
 
 //var uri = "https://www.google.com";
 //var psi = new System.Diagnostics.ProcessStartInfo();
 //psi.UseShellExecute = true;
 //psi.FileName = uri;
 //System.Diagnostics.Process.Start(psi);
+
+
+
+
+await new BrowserFetcher(Product.Firefox).DownloadAsync();
+await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+{
+    Headless = false,
+    Product = Product.Firefox
+});
+await using var page = await browser.NewPageAsync();
+await page.GoToAsync("https://www.ultraboardgames.com/qwirkle/game.php?startcomputer");
+
+//await page.WaitForSelectorAsync("score_player");
+var scorePlayer = await page.SelectAsync("score_player");
+//await page.ScreenshotAsync("./google.png");
+
+var links = @"Array.from(document.querySelectorAll('a')).map(a => a.href);";
+var urls = await page.EvaluateExpressionAsync<string[]>(links);
+
+
+List<string> programmerLinks = urls.ToList();
+
 
 var toto = 69;
