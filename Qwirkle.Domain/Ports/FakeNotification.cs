9b7efb42e7706784d17c105ddc9e5ab29558c776
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-05 18:12:34 +0100

    create fakes for DI

---
+++ Qwirkle.Domain/Ports/FakeNotification.cs
@@ -0,0 +1,29 @@
+﻿namespace Qwirkle.Domain.Ports;
+
+public class FakeNotification : INotification
+{
+    public void SendGameOver(int gameId, List<int> winnersPlayersIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SendPlayerIdTurn(int gameId, int getPlayerIdToPlay)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SendTilesPlayed(int gameId, int playerId, int scoredPoints, List<TileOnBoard> tilesOnBoardPlayed)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SendTilesSwapped(int gameId, int playerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SendTurnSkipped(int gameId, int playerId)
+    {
+        throw new NotImplementedException();
+    }
+}
