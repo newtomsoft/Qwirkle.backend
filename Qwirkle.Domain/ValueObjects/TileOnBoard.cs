e20e9e88dc2c091ef3f35108c8aa87c236d1bf8c
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-05 16:17:09 +0100

    refactoring

--- Qwirkle.Domain/ValueObjects/TileOnBoard.cs
+++ Qwirkle.Domain/ValueObjects/TileOnBoard.cs
@@ -1,14 +1,12 @@
 ﻿namespace Qwirkle.Domain.ValueObjects;
 
-public record TileOnBoard : Tile
+public record TileOnBoard(Tile Tile, Coordinates Coordinates) : Tile(Tile)
 {
-    public Coordinates Coordinates { get; }
-
-    public TileOnBoard(Tile tile, Coordinates coordinates) : base(tile) => Coordinates = coordinates;
-    public TileOnBoard(TileColor color, TileShape shape, Coordinates coordinates) : base(color, shape) => Coordinates = coordinates;
+    public TileOnBoard(TileColor color, TileShape shape, Coordinates coordinates) : this(new Tile(color, shape), coordinates) { }
     public static TileOnBoard From(TileOnPlayer tile, Coordinates coordinates) => new(tile, coordinates);
+    public static TileOnBoard From(Tile tile, Coordinates coordinates) => new(tile, coordinates);
 
-    private TileOnBoard(TileOnPlayer tile, Coordinates coordinates) : base(tile) => Coordinates = coordinates;
+    private TileOnBoard(TileOnPlayer tile, Coordinates coordinates) : this(tile.Color, tile.Shape, coordinates) { }
 
     public Tile ToTile() => new(Color, Shape);
 }
