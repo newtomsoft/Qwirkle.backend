6d09cce377ac00c96b6a998a258e8cd653e94aab
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-05 12:22:38 +0100

    methods GetTilesOnBag, GetPlayerPoints, GetBotPoints

--- Qwirkle.UltraBoardGames.Player/Program.cs
+++ Qwirkle.UltraBoardGames.Player/Program.cs
@@ -1,52 +1,16 @@
-﻿using HtmlAgilityPack;
-using PuppeteerSharp;
+﻿using Qwirkle.UltraBoardGames.Player;
 
 Console.WriteLine("scraping program");
 
-const string gamePageUrl = "https://www.ultraboardgames.com/qwirkle/game.php?startcomputer";
-//var web = new HtmlWeb();
-//var doc = web.Load(gamePageUrl);
-
-//< img src = "gamegfx/p4.png" class= "ui-draggable ui-draggable-handle" style = "position: relative;" >
-//class="ui-droppable"
-//var scorePlayer = int.Parse(doc.GetElementbyId("score_player").InnerText);
-//var scoreComputer = int.Parse(doc.GetElementbyId("score_computer").InnerText);
-
-//var process = new Process();
-//process.StartInfo.UseShellExecute = true;
-//process.StartInfo.FileName = url;
-//process.Start();
-
-//Process.Start(url);
-//Process.Start("nppp\\notepad++.exe", "args");
-
-//var uri = "https://www.google.com";
-//var psi = new System.Diagnostics.ProcessStartInfo();
-//psi.UseShellExecute = true;
-//psi.FileName = uri;
-//System.Diagnostics.Process.Start(psi);
 
 
 
-
-await new BrowserFetcher(Product.Firefox).DownloadAsync();
-await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
-{
-    Headless = false,
-    Product = Product.Firefox
-});
-await using var page = await browser.NewPageAsync();
-await page.GoToAsync("https://www.ultraboardgames.com/qwirkle/game.php?startcomputer");
-
-//await page.WaitForSelectorAsync("score_player");
-var scorePlayer = await page.SelectAsync("score_player");
-//await page.ScreenshotAsync("./google.png");
-
-var links = @"Array.from(document.querySelectorAll('a')).map(a => a.href);";
-var urls = await page.EvaluateExpressionAsync<string[]>(links);
+var scraper = new Scraper();
+var playerPoints = scraper.GetPlayerPoints();
+var botPoints = scraper.GetBotPoints();
+var tilesOnBag = scraper.GetTilesOnBag();
 
 
-List<string> programmerLinks = urls.ToList();
 
 
 var toto = 69;
--- Qwirkle.UltraBoardGames.Player/ScrapingPage.cs
+++ Qwirkle.UltraBoardGames.Player/ScrapingPage.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace Qwirkle.UltraBoardGames.Player;
+public class Scraper
+{
+    private readonly IWebDriver _driver;
+    private const string GamePageUrl = "https://www.ultraboardgames.com/qwirkle/game.php?startcomputer";
+    private const string DomScorePlayer = "score_player";
+    private const string DomScoreBot = "score_computer";
+    private const string DomBagStatus = "bag_status";
+
+    public Scraper()
+    {
+        _driver = new FirefoxDriver();
+        _ = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        _driver.Navigate().GoToUrl(GamePageUrl);
+    }
+
+    public int GetTilesOnBag()
+    {
+        //<span id="bag_status">96 tiles left</span>
+        var brutTilesOnBag = _driver.FindElement(By.Id(DomBagStatus)).Text;
+        return ConvertToInt(brutTilesOnBag);
+    }
+
+    public int GetPlayerPoints()
+    {
+        var brutPoints = _driver.FindElement(By.Id(DomScorePlayer)).Text;
+        return ConvertToInt(brutPoints, '(');
+    }
+
+    public int GetBotPoints()
+    {
+        var brutPoints = _driver.FindElement(By.Id(DomScoreBot)).Text;
+        return ConvertToInt(brutPoints, '(');
+    }
+
+
+    private static int ConvertToInt(string brutPoints, char ignoreAfter = ' ')
+    {
+        // brutPoints : "10 (4 last turn)" -> return 10
+        var indexToRemoveAfter = brutPoints.IndexOf(ignoreAfter);
+        var pointsString = indexToRemoveAfter > 0 ? brutPoints.Remove(indexToRemoveAfter) : brutPoints;
+        return int.Parse(pointsString);
+    }
+}
