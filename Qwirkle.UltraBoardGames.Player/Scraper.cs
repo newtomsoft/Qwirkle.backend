c0016864dcc3339ad4f7ef7d9fde2beaed867e70
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-05 16:18:26 +0100

    GetTilesOnPlayer, GetTilesOnBoard, refactoring

--- Qwirkle.UltraBoardGames.Player/Scraper.cs
+++ Qwirkle.UltraBoardGames.Player/Scraper.cs
@@ -1,15 +1,14 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
+using Qwirkle.Domain.ValueObjects;
 
 namespace Qwirkle.UltraBoardGames.Player;
 public class Scraper
 {
-    private readonly IWebDriver _driver;
     private const string GamePageUrl = "https://www.ultraboardgames.com/qwirkle/game.php?startcomputer";
-    private const string DomScorePlayer = "score_player";
-    private const string DomScoreBot = "score_computer";
-    private const string DomBagStatus = "bag_status";
+
+    private readonly IWebDriver _driver;
 
     public Scraper()
     {
@@ -20,29 +19,61 @@ public class Scraper
 
     public int GetTilesOnBag()
     {
-        //<span id="bag_status">96 tiles left</span>
-        var brutTilesOnBag = _driver.FindElement(By.Id(DomBagStatus)).Text;
+        var brutTilesOnBag = _driver.FindElement(By.Id("bag_status")).Text;
         return ConvertToInt(brutTilesOnBag);
     }
 
     public int GetPlayerPoints()
     {
-        var brutPoints = _driver.FindElement(By.Id(DomScorePlayer)).Text;
+        var brutPoints = _driver.FindElement(By.Id("score_player")).Text;
         return ConvertToInt(brutPoints, '(');
     }
 
-    public int GetBotPoints()
+    public int GetOpponentPoints()
     {
-        var brutPoints = _driver.FindElement(By.Id(DomScoreBot)).Text;
+        var brutPoints = _driver.FindElement(By.Id("score_computer")).Text;
         return ConvertToInt(brutPoints, '(');
+    }
+
+    public List<TileOnPlayer> GetTilesOnPlayer()
+    {
+        var tilesOnPlayer = new List<TileOnPlayer>();
+        for (int i = 0; i < 6; i++)
+        {
+            var fullImageName = _driver.FindElement(By.Id($"d{i}")).FindElement(By.TagName("img")).GetAttribute("src");
+            string imageCode = GetImageCode(fullImageName);
+            var tile = imageCode.ToTile();
+            tilesOnPlayer.Add(new TileOnPlayer((byte)(i + 1), tile));
+        }
+        return tilesOnPlayer;
+    }
+
+    public List<TileOnBoard> GetTilesOnBoard()
+    {
+        var tilesOnBoard = new List<TileOnBoard>();
+        var domTiles = _driver.FindElements(By.ClassName("ui-droppable")).ToList();
+        foreach (var domTile in domTiles)
+        {
+            try
+            {
+                var fullImageName = domTile.FindElement(By.TagName("img")).GetAttribute("src");
+                var coordinates = Coordinates.From(int.Parse(domTile.GetAttribute("mapx")), int.Parse(domTile.GetAttribute("mapy")));
+                tilesOnBoard.Add(TileOnBoard.From(GetImageCode(fullImageName).ToTile(), coordinates));
+            }
+            catch
+            {
+            }
+        }
+        return tilesOnBoard;
     }
 
 
     private static int ConvertToInt(string brutPoints, char ignoreAfter = ' ')
     {
-        // brutPoints : "10 (4 last turn)" -> return 10
         var indexToRemoveAfter = brutPoints.IndexOf(ignoreAfter);
         var pointsString = indexToRemoveAfter > 0 ? brutPoints.Remove(indexToRemoveAfter) : brutPoints;
         return int.Parse(pointsString);
+        // brutPoints : "10 (4 last turn)" -> return 10
     }
+    private static string GetImageCode(string fullImageName) => fullImageName[(fullImageName.LastIndexOf('/') + 1)..fullImageName.LastIndexOf('.')];
 }
