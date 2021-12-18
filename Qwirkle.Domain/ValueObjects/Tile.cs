098706d32de14848a0a173f37038d2629854b3cf
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-18 15:45:49 +0100

    add ToTileOnPlayer

--- Qwirkle.Domain/ValueObjects/Tile.cs
+++ Qwirkle.Domain/ValueObjects/Tile.cs
@@ -5,4 +5,6 @@ public record Tile(TileColor Color, TileShape Shape)
     protected Tile(Tile tile) => (Color, Shape) = tile;
 
     public bool OnlyShapeOrColorEqual(Tile tile) => Color == tile.Color && Shape != tile.Shape || Color != tile.Color && Shape == tile.Shape;
+
+    public TileOnPlayer ToTileOnPlayer(RackPosition rackPosition) => new(rackPosition, this);
 }
