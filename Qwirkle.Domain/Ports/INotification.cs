73f2589fc25a7350cdac2dbe0514121fb3a7e05b
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-26 12:54:37 +0100

    rename parameter

--- Qwirkle.Domain/Ports/INotification.cs
+++ Qwirkle.Domain/Ports/INotification.cs
@@ -3,7 +3,7 @@
 public interface INotification
 {
     void SendTurnSkipped(int gameId, int playerId);
-    void SendPlayerIdTurn(int gameId, int getPlayerIdToPlay);
+    void SendPlayerIdTurn(int gameId, int playerId);
     void SendTilesPlayed(int gameId, int playerId, int scoredPoints, List<TileOnBoard> tilesOnBoardPlayed);
     void SendTilesSwapped(int gameId, int playerId);
     void SendGameOver(int gameId, List<int> winnersPlayersIds);
