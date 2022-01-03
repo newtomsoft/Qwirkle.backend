c69234cca15f78daa4de4a45718a728c17ea6166
Thomas Vuille <thomas@newtomsoft.fr>
2022-01-03 10:47:21 +0100

    Player Get refactoring and renaming

--- Qwirkle.Web.Api/Controllers/PlayerController.cs
+++ Qwirkle.Web.Api/Controllers/PlayerController.cs
@@ -21,20 +21,21 @@ public class PlayerController : ControllerBase
     public ActionResult GetById(int playerId) => new ObjectResult(_infoUseCase.GetPlayer(playerId));
 
 
-    [Obsolete("method soon to be discontinued")]
-    [HttpGet("{gameId}/{userId:int}")]
-    public ActionResult GetByGameIdUserId(int gameId, int userId) => new ObjectResult(_infoUseCase.GetPlayer(gameId, userId));
+    [HttpGet("ByGameId/{gameId:int}")]
+    public ActionResult GetByGameId(int gameId) => new ObjectResult(_infoUseCase.GetPlayer(gameId, UserId));
 
 
     [HttpGet("PlayerIdByGameId/{gameId:int}")]
-    public ActionResult GetByGameId(int gameId) => new ObjectResult(_infoUseCase.GetPlayer(gameId, UserId).Id);
+    public ActionResult GetIdByGameId(int gameId) => new ObjectResult(_infoUseCase.GetPlayer(gameId, UserId).Id);
 
 
     [HttpGet("NameTurn/{gameId:int}")]
     public ActionResult GetNameTurn(int gameId) => new ObjectResult(_infoUseCase.GetPlayerNameTurn(gameId));
 
+
     [HttpGet("IdTurn/{gameId:int}")]
     public ActionResult GetIdTurn(int gameId) => new ObjectResult(_infoUseCase.GetPlayerIdTurn(gameId));
+
 
     [HttpGet("Winners/{gameId:int}")]
     public ActionResult Winners(int gameId) => new ObjectResult(_infoUseCase.GetWinnersPlayersId(gameId));
