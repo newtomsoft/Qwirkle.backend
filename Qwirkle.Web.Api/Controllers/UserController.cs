8c53ef50ec970cf94478d9baf2cbc1a48f181c7e
Thomas Vuille <thomas@newtomsoft.fr>
2022-01-05 16:43:36 +0100

    create bot artefact to change

--- Qwirkle.Web.Api/Controllers/UserController.cs
+++ Qwirkle.Web.Api/Controllers/UserController.cs
@@ -7,10 +7,12 @@ public class UserController : ControllerBase
 {
     private readonly AuthenticationUseCase _authenticationUseCase;
 
-    public UserController(AuthenticationUseCase authenticationUseCase)
-    {
-        _authenticationUseCase = authenticationUseCase;
-    }
+    public UserController(AuthenticationUseCase authenticationUseCase) => _authenticationUseCase = authenticationUseCase;
+
+    [Authorize]
+    [HttpGet("CreateBots")]
+    public async Task CreateBotsAsync() => await _authenticationUseCase.CreateBots();
+
 
     [AllowAnonymous]
     [HttpPost("Register")]
@@ -21,6 +23,14 @@ public class UserController : ControllerBase
     [HttpPost("Login")]
     public async Task<ActionResult> LoginAsync(LoginViewModel login) => IsAuthenticated() ? AlreadyAuthenticated() : new ObjectResult(await _authenticationUseCase.LoginAsync(login.Pseudo, login.Password, login.IsRemember));
 
+    [AllowAnonymous]
+    [HttpGet("RegisterGuest")]
+    public async Task<ActionResult> RegisterGuestAsync()
+    {
+        var result = await _authenticationUseCase.RegisterGuest();
+        
+        return new ObjectResult(result);
+    }
 
     [Obsolete]
     [HttpGet("WhoAmI")]
