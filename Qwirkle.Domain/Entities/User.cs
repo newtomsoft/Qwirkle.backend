4246b0280cb5bab27d1f7d22e72c64b402377d2b
Thomas Vuille <thomas@newtomsoft.fr>
2022-01-06 16:57:25 +0100

    add User constructor

--- Qwirkle.Domain/Entities/User.cs
+++ Qwirkle.Domain/Entities/User.cs
@@ -3,12 +3,26 @@
 public class User
 {
     public string Pseudo { get; set; }
+    public string Email { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Email { get; set; }
     public int Help { get; set; }
     public int Points { get; set; }
     public int GamesPlayed { get; set; }
     public int GamesWon { get; set; }
     public List<Player> Players { get; set; }
+
+
+    public User(string pseudo, string email, string firstName = default, string lastName = default, int help = 0, int points = 0, int gamesPlayed = 0, int gamesWon = 0, Player player = default)
+    {
+        Pseudo = pseudo;
+        Email = email;
+        FirstName = firstName;
+        LastName = lastName;
+        Help = help;
+        Points = points;
+        GamesPlayed = gamesPlayed;
+        GamesWon = gamesWon;
+        Players = new List<Player> { player };
+    }
 }
