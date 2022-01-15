4c5001613b1acb52c20db7dee5d5c89ddb7de74c
Thomas Vuille <thomas@newtomsoft.fr>
2022-01-15 21:59:07 +0100

    rename fields and var

--- Qwirkle.Domain/Services/InstantGameService.cs
+++ Qwirkle.Domain/Services/InstantGameService.cs
@@ -7,21 +7,21 @@ public class InstantGameService
     private readonly INotification _notification;
     private readonly ILogger<InstantGameService> _logger;
 
-    private readonly Dictionary<int, HashSet<int>> _instantGames;
+    private readonly Dictionary<int, HashSet<int>> _instantGamesUsers;
 
     public InstantGameService(INotification notification, ILogger<InstantGameService> logger)
     {
         _notification = notification;
         _logger = logger;
-        _instantGames = new Dictionary<int, HashSet<int>> { { 2, new HashSet<int>(2) }, { 3, new HashSet<int>(3) }, { 4, new HashSet<int>(4) } };
+        _instantGamesUsers = new Dictionary<int, HashSet<int>> { { 2, new HashSet<int>(2) }, { 3, new HashSet<int>(3) }, { 4, new HashSet<int>(4) } };
     }
 
     public HashSet<int> JoinInstantGame(int userId, int playersNumber)
     {
-        _logger?.LogInformation($"userId:{userId} {MethodBase.GetCurrentMethod()!.Name} with {_instantGames[playersNumber]}");
-        _instantGames[playersNumber].Add(userId);
-        var instantGames = new HashSet<int>(_instantGames[playersNumber]);
-        if (_instantGames[playersNumber].Count == playersNumber) _instantGames[playersNumber] = new HashSet<int>();
-        return instantGames;
+        _logger?.LogInformation($"userId:{userId} {MethodBase.GetCurrentMethod()!.Name} with {_instantGamesUsers[playersNumber]}");
+        _instantGamesUsers[playersNumber].Add(userId);
+        var usersIds = new HashSet<int>(_instantGamesUsers[playersNumber]);
+        if (_instantGamesUsers[playersNumber].Count == playersNumber) _instantGamesUsers[playersNumber] = new HashSet<int>();
+        return usersIds;
     }
 }
