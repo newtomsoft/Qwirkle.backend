67d6add5177760e25078066147943f27c4053131
Thomas Vuille <thomas@newtomsoft.fr>
2022-02-02 10:19:16 +0100

    remove braces

--- Qwirkle.Domain/ExtensionMethods/TilesExtensionsMethods.cs
+++ Qwirkle.Domain/ExtensionMethods/TilesExtensionsMethods.cs
@@ -35,9 +35,8 @@ public static class TilesExtensionsMethods
     {
         var stringBuilder = new StringBuilder();
         foreach (var tile in tiles)
-        {
             stringBuilder.Append($"{tile.Shape}-{tile.Color} ({tile.Coordinates.X},{tile.Coordinates.Y}) ");
-        }
+
         return stringBuilder.ToString();
     }
 }
